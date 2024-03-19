8966536f63f5e26af4a04965369cbffdfb5a5824
Wilmaryucuma7 <wcjs71014@hotmail.com>
2024-03-19 14:59:52 -0500

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  username = 'wilmar';
+  isLoggedIn = false;
+
+  greet() {
+    alert('Hello ');
+  }
+}
